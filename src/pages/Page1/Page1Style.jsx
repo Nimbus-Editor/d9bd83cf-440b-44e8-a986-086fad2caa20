5feb29ac13ad31c246926688018ab84508939d77
JakeDurante <jdurantt0@gmail.com>
2022-10-08 08:26:02 -0400

    init commit

---
+++ src/pages/Page1/Page1Style.jsx
@@ -0,0 +1,20 @@
+const Page1Style = theme => ({
+  root: {
+    flex: 1,
+    flexWrap: 'wrap',
+    minHeight: '100vh',
+    backgroundColor: '#181A1E',
+  },
+
+  Container1: {
+    backgroundColor: '#9013fe',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    width: '282px',
+    height: '275px',
+  },
+})
+
+export default Page1Style;
